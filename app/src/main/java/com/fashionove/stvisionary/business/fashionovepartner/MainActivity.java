bfb17613e852d3461446262f5b9e5b1943c826a1
Tarique Shamsi <tariqueshamsi252@gmail.com>
2016-01-03 01:11:49 +0530

    Basic Layout and Add Contacts

--- app/src/main/java/com/fashionove/stvisionary/business/fashionovepartner/MainActivity.java
+++ app/src/main/java/com/fashionove/stvisionary/business/fashionovepartner/MainActivity.java
@@ -1,17 +1,41 @@
 package com.fashionove.stvisionary.business.fashionovepartner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Fresco.initialize(this);
         setContentView(R.layout.activity_main);
+        Button sms = (Button)findViewById(R.id.sms_button);
+        sms.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+                Intent activityChangeIntent = new Intent(MainActivity.this, ChooseTemplate.class);
+
+                // currentContext.startActivity(activityChangeIntent);
+
+                MainActivity.this.startActivity(activityChangeIntent);
+            }
+        });
     }
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
