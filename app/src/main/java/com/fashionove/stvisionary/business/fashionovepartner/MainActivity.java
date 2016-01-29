8be2866d32a5040c0e6c6452686caa4524630611
Tarique Shamsi <tariqueshamsi252@gmail.com>
2016-01-29 17:43:32 +0530

    add internet permission in manifest

--- app/src/main/java/com/fashionove/stvisionary/business/fashionovepartner/MainActivity.java
+++ app/src/main/java/com/fashionove/stvisionary/business/fashionovepartner/MainActivity.java
@@ -1,6 +1,10 @@
 package com.fashionove.stvisionary.business.fashionovepartner;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,9 +12,33 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.Toast;
+
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private CallbackManager mCallbackManager;
+    List<String> permissionNeeds = Arrays.asList("email", "user_friends");
+    private Map<String, String> params = new HashMap<String, String>();
+    private Button facebookLogin;
+    private boolean facebook = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +46,18 @@ public class MainActivity extends AppCompatActivity {
 
         Fresco.initialize(this);
         setContentView(R.layout.activity_main);
-        Button sms = (Button)findViewById(R.id.sms_button);
+
+
+        Button upgrade = (Button) findViewById(R.id.upgrade_button);
+
+        upgrade.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showSoftUpdateDialog();
+            }
+        });
+
+
+        Button sms = (Button) findViewById(R.id.sms_button);
         sms.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
@@ -30,11 +69,19 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(activityChangeIntent);
             }
         });
+
+        facebookLogin = (Button) findViewById(R.id.fb_button);
+        facebookLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                facebook = true;
+                doTheFacebookLogin();
+            }
+        });
+
+
     }
-
-
-
-
 
 
     @Override
@@ -58,4 +105,87 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void doTheFacebookLogin() {
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().logInWithReadPermissions(this, permissionNeeds);
+        LoginManager.getInstance().registerCallback(mCallbackManager, mCallback);
+    }
+
+    private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+
+            Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+
+
+            sharePhotoToFacebook();
+
+
+        }
+
+        @Override
+        public void onCancel() {
+            // customToast("you cancelled");
+            Toast.makeText(getApplicationContext(), "Please check your data Connection", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+            //   customToast("Oops! Something went wrong, Try again.");
+        }
+
+    };
+
+    private void sharePhotoToFacebook() {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/jpeg");
+
+        share.putExtra(Intent.EXTRA_STREAM, Uri.parse(
+                "file:///mnt/sdcard/test/v1327056571768.3gpp"));
+
+        startActivity(Intent.createChooser(share, "Share Image"));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent data) {
+        super.onActivityResult(requestCode, responseCode, data);
+        mCallbackManager.onActivityResult(requestCode, responseCode, data);
+    }
+
+
+    //soft update of the app
+    public void showSoftUpdateDialog() {
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(this, AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+        builder1.setTitle("Update");
+        builder1.setMessage("A new version is available. Please update the App.");
+        builder1.setCancelable(true);
+        builder1.setPositiveButton("UPDATE",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        //take to the play store page
+                        final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+                        try {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                        } catch (android.content.ActivityNotFoundException anfe) {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + appPackageName)));
+                        }
+
+                    }
+                });
+        builder1.setNegativeButton("LATER",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
 }
+
